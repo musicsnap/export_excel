84859c0e2e54b24db218e1d9c679ab703a9b7a0b
music <musicsnap@163.com>
2019-03-25 15:57:52 +0800

    no message

--- tests/default_test.go
+++ tests/default_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"runtime"
 	"path/filepath"
-	_ "excel/export_excel/routers"
+	_ "export_excel/routers"
 
 	"github.com/astaxie/beego"
 	. "github.com/smartystreets/goconvey/convey"
